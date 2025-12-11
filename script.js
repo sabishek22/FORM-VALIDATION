59051981cafb179547dc4af47c947f30c41d35c9
sabishek22 <s.abishekabi22@gmail.com>
2025-12-11 23:08:30 +0530

    completed

--- script.js
+++ script.js
@@ -1,70 +1,87 @@
+let form = document.getElementById("register-form");
+let userName = document.getElementById("nameBox");
+let email = document.getElementById("mailBox");
+let password = document.getElementById("passwordBox");
+let submit = document.getElementById("btnRegister");
 
-  let form = document.getElementById("register-form")
-  let name = document.getElementById("nameBox");
-  let email = document.getElementById("mailBox");
-  let password = document.getElementById("passwordBox");
-  let submit = document.getElementById("btnRegister");
+let nameError = document.getElementById("nameError");
+let mailError = document.getElementById("mailError");
+let passError = document.getElementById("passError");
 
-  let nameError = document.getElementById("nameError");
-  let mailError = document.getElementById("mailError");
-  let passError = document.getElementById("passError");
+let touched = {
+  name: false,
+  email: false,
+  password: false,
+};
 
-  function validateForm(){
-
+function validateForm() {
   let validate = true;
 
+  let nameRegex = /^[a-zA-Z ]+$/;
 
-  if (name.value.trim() === "") {
-    nameError.textContent = "Name connot be Empty!";
-    validate = false;
-  } else {
-    nameError.textContent = "";
+  if (touched.name) {
+    if (userName.value.trim() === "") {
+      nameError.textContent = "Name cannot be Empty!";
+      validate = false;
+    } else if (nameRegex.test(userName.value.trim()) == false) {
+      nameError.textContent = "Enter a valid Name";
+      validate = false;
+    } else {
+      nameError.textContent = "";
+    }
   }
 
   let emailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
 
-
-  if (emailRegex.test(email.value.trim()) == false) {
-    mailError.textContent = "Enter a valid Gmail id";
-    validate = false;
-  } else {
-    mailError.textContent = "";
+  if (touched.email) {
+    if (email.value.trim() === "") {
+      mailError.textContent = "Email cannot be empty";
+      validate = false;
+    } else if (emailRegex.test(email.value.trim()) == false) {
+      mailError.textContent = "Enter a valid Gmail id";
+      validate = false;
+    } else {
+      mailError.textContent = "";
+    }
   }
 
+  if (touched.password) {
+    if (password.value.length < 6) {
+      passError.textContent =
+        "Your password must contains atleast 6 characters";
 
-  if (password.value.length < 6) {
-    passError.textContent = "Your password must contains atleast 6 characters";
-
-    validate = false;
-  } else {
-    passError.textContent = "";
+      validate = false;
+    } else {
+      passError.textContent = "";
+    }
   }
 
   submit.disabled = !validate;
 
-  return(validate)
+  return validate;
 }
 
+userName.addEventListener("input",()=>{touched.name = true; validateForm()})
+email.addEventListener("input",()=>{touched.email = true; validateForm() })
+password.addEventListener("input",()=>{touched.password = true; validateForm()})
 
-name.addEventListener("input", validateForm);
-email.addEventListener("input",validateForm);
-password.addEventListener("input",validateForm)
+submit.addEventListener("click", function (event) {
+  event.preventDefault();
 
+  touched.name = touched.email = touched.password = true;
 
-submit.addEventListener("click",function(event){
-    event.preventDefault();
+  if (validateForm()) {
+    alert("Form Submitted Successfully");
 
-    if (validateForm()) {
-    alert("Form Submitted Sucefully");
- 
     form.reset();
 
     nameError.textContent = "";
     mailError.textContent = "";
     passError.textContent = "";
-
     submit.disabled = true;
-    
-  }
 
-})
+    touched.name = false;
+    touched.email = false;
+    touched.password = false;
+  }
+});
